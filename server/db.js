1c1aa4c1b0df11ca3162088129019cf027259e94
gretchenplant <gretchen.e.plant@gmail.com>
2024-10-22 22:18:45 -0400

    mongo db server creation

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:nM5NyQqyAbEDES6DUXTrK0rm@172.21.42.152:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
